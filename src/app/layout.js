94c924a85bff5ade162c7acf28029d8682151722
Chris Timbol <christimbol707@gmail.com>
2023-11-15 18:33:11 -0800

    Seperated the layout to fetch nonce initially

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-
 import './globals.css'
 import { Cinzel } from 'next/font/google'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import CartProvider from '@/components/Context';
 
 const cinzel = Cinzel(
   {
@@ -13,29 +13,21 @@ const cinzel = Cinzel(
   },
 )
 
-export default async function RootLayout({ children }) {
-  /* Fetch nonce for cart */
-  const res = await fetch('http://restarauntwoo.local/wp-json/wc/store/v1/cart');
-  const nonce = res.headers.get('Nonce');
-  const cartData = await res.json()
-  const cartCount = cartData.items_count
-  /*   const [cartCount, setCartCount] = useState(0); */
+export default async function RootLayout( props ) {
 
+  /* Nonce must be fetched on server component */
+  const res = await fetch('http://restarauntwoo.local/wp-json/wc/store/v1/cart')
+  const fetchedNonce = res.headers.get('Nonce') 
+  console.log(props)
   return (
     <html lang="en" className={cinzel.className}>
       <body className="" >
- {/*        <CartProvider> */}
-          <Header nonce={nonce} cartCount={cartCount} />
-
-          {children}
-   {/*        </CartProvider> */}
+        <CartProvider>
+          <Header nonce={fetchedNonce} />
+          {props.children}
           <Footer />
-      
+        </CartProvider>
       </body>
     </html>
   )
 }
-
-// Create cart context
-// pass the context to children? then use it to add items to cart
-// use it to store cart and fetch cookies initaly etc
