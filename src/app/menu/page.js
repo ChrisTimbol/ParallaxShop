21802a257d5f267b3ecf36a66ba0f8abde4489c6
Chris Timbol <christimbol707@gmail.com>
2023-11-15 18:36:06 -0800

    Modulate pages for fetching nonce headers

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,14 +1,19 @@
 import MenuClient from '@/components/MenuClient'
 
 export default async function Menu() {
-    const res = await fetch('http://restarauntwoo.local/wp-json/wc/store/v1/cart', { cache: 'no-store' } );
+    const res = await fetch('http://restarauntwoo.local/wp-json/wc/store/v1/cart', { cache: 'no-store' });
     const nonce = res.headers.get('Nonce')
-    const cartToken = res.headers.get('Cart-Token');
+   // const cartToken = res.headers.get('Cart-Token');
+
+/*     
+    const products = await fetch('http://restarauntwoo.local/wp-json/wc/store/v1/products')
+    const categories = await fetch('http://restarauntwoo.local/wp-json/wc/store/v1/products/categories')     
+*/
+
     return (
         <>
             <div className="py-32"></div>
-                <MenuClient nonce={nonce} cartToken={cartToken} />
-
+            <MenuClient nonce={nonce}  />
         </>
     )
 }
